fa520849171fb76e91aac5d2b03660227dfc6615
Gavin Vaske <gavin.vaske@gmail.com>
2019-12-01 20:22:14 -0600

    initial commit

---
+++ Pods/Target Support Files/Pods-data-preservation/Pods-data-preservation-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_data_preservation : NSObject
+@end
+@implementation PodsDummy_Pods_data_preservation
+@end
---
+++ data-preservation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  data-preservation
+//
+//  Created by Vaske, Gavin J on 12/1/19.
+//  Copyright © 2019 Vaske, Gavin J. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
